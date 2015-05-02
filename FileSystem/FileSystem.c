82524b1d0c51d4b5a57af686a8adc90f40ad2fbf
Tomas Yagas <tomas.yagas@gmail.com>
2015-05-02 01:27:32 -0300

    creo logger y levanto archivo de config en FS

--- FileSystem/FileSystem.c
+++ FileSystem/FileSystem.c
@@ -5,4 +5,41 @@
  *      Author: utnso
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/config.h>
+#include <commons/log.h>
 
+int PUERTO_LISTEN;
+char** LISTA_NODOS;
+t_log* LOGGER;
+bool STATUS;
+
+void levantar_configuracion() {
+	t_config* config;
+	config = config_create("config_file_system.cfg");
+	if (config_has_property(config, "PUERTO_LISTEN")) {
+		PUERTO_LISTEN = config_get_int_value(config, "PUERTO_LISTEN");
+	}
+	if (config_has_property(config, "LISTA_NODOS")) {
+		LISTA_NODOS = config_get_array_value(config, "LISTA_NODOS");
+	}
+	config_destroy(config);
+}
+
+void crear_logger() {
+	LOGGER = log_create("log", "FileSystem", true, LOG_LEVEL_INFO);
+}
+
+int main(void) {
+	levantar_configuracion();
+	STATUS = false;
+	crear_logger();
+	log_info(LOGGER,"Puerto listen: %d\n",PUERTO_LISTEN);
+	printf("Lista: %s\n",&LISTA_NODOS);
+	printf("Lista: %s\n",LISTA_NODOS);
+
+
+	log_destroy(LOGGER);
+	return EXIT_SUCCESS;
+}
