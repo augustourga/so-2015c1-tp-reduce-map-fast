818dad06aa97516e92fd6cb399ffea8b7929d873
utnso <utnso@utnso>
2015-05-15 02:51:34 -0300

    Creo hilo conexion_FileSystem.

--- Nodo/Nodo.h
+++ Nodo/Nodo.h
@@ -16,8 +16,15 @@
 #include <stdlib.h>
 #include "sockets.h"
 #include <commons/config.h>
-#define PATH "/home/utnso/workspace/NODO_CONFIG.config"
+#include <commons/log.h>
+#include <pthread.h>
 
+/******************Constantes********************/
+#define PATH "/home/utnso/git/tp-2015-1c-milanesa/Nodo/NODO_CONFIG.config"
+#define LOG_FILE "/home/utnso/Job_log.txt"
+#define PROCESO "Nodo"
+
+/****************Registros***********************/
 typedef struct{
 	int sock_fs;
 }t_conexion_nodo;
@@ -26,6 +33,7 @@ typedef struct{
 	int sock_fs;
 }t_bloque_content;
 
+/******************Variables************************/
 		 int PUERTO_FS;
 		 char* IP_FS;
 		 char* ARCHIVO_BIN;
@@ -33,10 +41,15 @@ typedef struct{
 		 char* NODO_NUEVO;
 		 char* IP_NODO;
 	     int PUERTO_NODO;
+	     t_log* Log_Nodo;
+         int rcx;
+/******************Definiciones*******************/
 
-
-void levantarConfiguracionNodo();
+int levantarConfiguracionNodo();
 void conectarFileSystem();
  //getBloque(unNumero);
 int levantarServer();
+int levantarHiloFile();
+
+
 #endif /* NODO_NODO_H_ */
