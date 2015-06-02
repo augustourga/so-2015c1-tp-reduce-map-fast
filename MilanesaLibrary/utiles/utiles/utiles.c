4472d01db9abe23dc7b748ba570411166c41f2b9
AlexanderPoschenrieder <aposchenrieder@yahoo.com.ar>
2015-06-02 00:25:19 -0300

    Agrego la carpeta útiles que podemos utilizar para serializar conexiones de manera mas genérica.

---
+++ MilanesaLibrary/utiles/utiles/utiles.c
@@ -0,0 +1,716 @@
+#include "utiles.h"
+
+int server_socket(uint16_t port)
+{
+	int sock_fd, optval = 1;
+	struct sockaddr_in servername;
+
+	/* Create the socket. */
+	sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock_fd < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	/* Set socket options. */
+	if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval) == -1) {
+		perror("setsockopt");
+		return -2;
+	}
+
+	/* Fill ip / port info. */
+	servername.sin_family = AF_INET;
+	servername.sin_addr.s_addr = htonl(INADDR_ANY);
+	servername.sin_port = htons(port);
+
+ 	/* Give the socket a name. */
+	if (bind(sock_fd, (struct sockaddr *) &servername, sizeof servername) < 0) {
+		perror("bind");
+		return -3;
+	}
+
+	/* Listen to incoming connections. */
+	if (listen(sock_fd, 1) < 0) {
+		perror("listen");
+		return -4;
+	}
+
+	return sock_fd;
+}
+
+
+int client_socket(char *ip, uint16_t port)
+{
+	int sock_fd;
+	struct sockaddr_in servername;
+
+	/* Create the socket. */
+	sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock_fd < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	/* Fill server ip / port info. */
+	servername.sin_family = AF_INET;
+	servername.sin_addr.s_addr = inet_addr(ip);
+	servername.sin_port = htons(port);
+	memset(&(servername.sin_zero), 0, 8);
+
+	/* Connect to the server. */
+	if (connect(sock_fd, (struct sockaddr *) &servername, sizeof servername) < 0) {
+		perror("connect");
+		return -2;
+	}
+
+	return sock_fd;
+}
+
+
+int accept_connection(int sock_fd)
+{
+	struct sockaddr_in clientname;
+	size_t size = sizeof clientname;
+
+	int new_fd = accept(sock_fd, (struct sockaddr *) &clientname, (socklen_t *) &size);
+	if (new_fd < 0) {
+		perror("accept");
+		return -1;
+	}	
+
+	return new_fd;
+}
+
+
+t_msg *id_message(t_msg_id id) {
+
+	t_msg *new = malloc(sizeof *new);
+
+	new->header.id = id;
+	new->argv = NULL;
+	new->stream = NULL;
+	new->header.argc = 0;
+	new->header.length = 0;
+	
+	return new;
+}
+
+
+t_msg *argv_message(t_msg_id id, uint16_t count, ...)
+{
+	va_list arguments;
+	va_start(arguments, count);
+
+	int32_t *val = malloc(count * sizeof *val);
+
+	int i;
+	for (i = 0; i < count; i++) {
+		val[i] = va_arg(arguments, uint32_t);
+	}
+
+	t_msg *new = malloc(sizeof *new);
+	new->header.id = id;
+	new->header.argc = count;
+	new->argv = val;
+	new->header.length = 0;
+	new->stream = NULL;
+
+	va_end(arguments);
+
+	return new;
+}
+
+
+t_msg *string_message(t_msg_id id, char *message, uint16_t count, ...)
+{
+	va_list arguments;
+	va_start(arguments, count);
+
+	int32_t *val = NULL;
+	if(count > 0) 
+		val = malloc(count * sizeof *val);
+
+	int i;
+	for (i = 0; i < count; i++) {
+		val[i] = va_arg(arguments, uint32_t);
+	}
+
+	t_msg *new = malloc(sizeof *new);
+	new->header.id = id;
+	new->header.argc = count;
+	new->argv = val;
+	new->header.length = strlen(message);
+	new->stream = string_duplicate(message);
+
+	va_end(arguments);
+
+	return new;
+}
+
+
+t_msg *modify_message(t_msg_id new_id, t_msg *old_msg, uint16_t new_count, ...)
+{
+	va_list arguments;
+	va_start(arguments, new_count);
+
+	uint16_t old_count = old_msg->header.argc;
+
+	int32_t *val = malloc((new_count + old_count) * sizeof *val);
+
+	int i;
+	for (i = 0; i < new_count; i++) {
+		val[i] = va_arg(arguments, uint32_t);
+	}
+
+	memcpy(val + new_count, old_msg->argv, old_count * sizeof(uint32_t));
+
+
+	char *buffer = NULL;
+	if(old_msg->header.length > 0)
+		buffer = malloc(old_msg->header.length);
+
+	memcpy(buffer, old_msg->stream, old_msg->header.length);
+
+	t_msg *new = malloc(sizeof *new);
+	new->header.id = new_id == NO_NEW_ID ? old_msg->header.id : new_id;
+	new->header.argc = new_count + old_count;
+	new->argv = val;
+	new->header.length = old_msg->header.length;
+	new->stream = buffer;
+
+	va_end(arguments);
+
+	destroy_message(old_msg);
+
+	return new;
+}
+
+
+t_msg *remake_message(t_msg_id new_id, t_msg *old_msg, uint16_t new_count, ...)
+{
+	va_list arguments;
+	va_start(arguments, new_count);
+
+	int32_t *val = NULL;
+	if(new_count > 0)
+		val = malloc(new_count * sizeof *val);
+
+	int i;
+	for (i = 0; i < new_count; i++) {
+		val[i] = va_arg(arguments, uint32_t);
+	}
+
+	char *buffer = NULL;	
+	if(old_msg->header.length > 0) {
+		buffer = malloc(old_msg->header.length);
+		memcpy(buffer, old_msg->stream, old_msg->header.length);
+	}
+
+
+	t_msg *new = malloc(sizeof *new);
+	new->header.id = new_id == NO_NEW_ID ? old_msg->header.id : new_id;
+	new->header.argc = new_count;
+	new->argv = val;
+	new->header.length = old_msg->header.length;
+	new->stream = buffer;
+
+	va_end(arguments);
+
+	destroy_message(old_msg);
+
+	return new;
+}
+
+
+t_msg *beso_message(t_msg_id id, char *beso_path, uint16_t count, ...)
+{
+	va_list arguments;
+	va_start(arguments, count);
+
+	int32_t *val = NULL;
+	if(count > 0)
+		val = malloc(count * sizeof *val);
+
+	int i;
+	for (i = 0; i < count; i++) {
+		val[i] = va_arg(arguments, uint32_t);
+	}
+
+	FILE *f = fopen(beso_path, "rb");
+	if (f == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+	
+	fseek(f, 0, SEEK_END);
+	long fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	char *buffer = malloc(fsize);
+
+	fread(buffer, fsize, 1, f);
+
+	t_msg *new = malloc(sizeof *new);
+	new->header.id = id;
+	new->header.argc = count;
+	new->argv = val;
+	new->header.length = fsize;
+	new->stream = buffer;
+
+	va_end(arguments);
+
+	fclose(f);
+
+	return new;
+}
+
+
+t_msg *tcb_message(t_msg_id id, t_hilo *tcb, uint16_t count, ...)
+{
+	va_list arguments;
+	va_start(arguments, count);
+
+	int32_t *val = malloc(count * sizeof *val);
+
+	int i;
+	for (i = 0; i < count; i++) {
+		val[i] = va_arg(arguments, uint32_t);
+	}
+
+	size_t size = sizeof *tcb;
+	char *buffer = malloc(size);
+
+	memcpy(buffer, tcb, size);
+
+	t_msg *new = malloc(sizeof *new);
+	new->header.id = id;
+	new->header.argc = count;
+	new->argv = val;
+	new->header.length = size;
+	new->stream = buffer;
+
+	va_end(arguments);
+
+	return new;
+}
+
+
+t_msg *recibir_mensaje(int sock_fd)
+{
+	t_msg *msg = malloc(sizeof *msg);
+	msg->argv = NULL;
+	msg->stream = NULL;
+
+
+ 	/* Get message info. */
+	int status = recv(sock_fd, &(msg->header), sizeof(t_header), MSG_WAITALL);
+	if (status <= 0) {
+ 		/* An error has ocurred or remote connection has been closed. */
+		free(msg);
+		return NULL;
+	}
+
+ 	/* Get message data. */
+	if (msg->header.argc > 0) {
+		msg->argv = malloc(msg->header.argc * sizeof(uint32_t));
+
+		if (recv(sock_fd, msg->argv, msg->header.argc * sizeof(uint32_t), MSG_WAITALL) <= 0) {
+			free(msg->argv);
+			free(msg);
+			return NULL;
+		}
+	}
+
+	if (msg->header.length > 0) {
+		msg->stream = malloc(msg->header.length + 1);
+
+		if (recv(sock_fd, msg->stream, msg->header.length, MSG_WAITALL) <= 0) {
+			free(msg->stream);		
+			free(msg->argv);
+			free(msg);
+			return NULL;
+		}
+		
+		msg->stream[msg->header.length] = '\0';
+	}
+
+
+	return msg;
+}
+
+
+int enviar_mensaje(int sock_fd, t_msg *msg)
+{
+	int total = 0;
+	int pending = msg->header.length + sizeof(t_header) + msg->header.argc * sizeof(uint32_t);
+	char *buffer = malloc(pending);
+
+ 	/* Fill buffer with the struct's data. */
+	memcpy(buffer, &(msg->header), sizeof(t_header));
+
+	int i;
+	for (i = 0; i < msg->header.argc; i++)
+		memcpy(buffer + sizeof(t_header) + i * sizeof(uint32_t), msg->argv + i, sizeof(uint32_t));
+
+	memcpy(buffer + sizeof(t_header) + msg->header.argc * sizeof(uint32_t), msg->stream, msg->header.length);
+
+ 	/* Send message(s). */
+	while (total < pending) {
+		int sent = send(sock_fd, buffer, msg->header.length + sizeof msg->header + msg->header.argc * sizeof(uint32_t), MSG_NOSIGNAL);
+		if (sent < 0) {
+			free(buffer);
+			return -1;
+		}
+		total += sent;
+		pending -= sent;
+	}
+
+	free(buffer);
+
+	return total;
+}
+
+
+void destroy_message(t_msg *msg)
+{
+	if(msg->header.length && msg->stream != NULL)
+		free(msg->stream);
+	if(msg->header.argc && msg->argv != NULL)
+		free(msg->argv);
+	free(msg);
+}
+
+void seedgen(void)
+{
+	long seed;
+	time_t seconds;
+	seed = abs(((time(&seconds) * 181) * ((getpid() - 83) * 359)) % 104729);
+	srand(seed);
+}
+
+
+t_hilo *retrieve_tcb(t_msg *msg)
+{
+	t_hilo *new = malloc(sizeof *new);
+	memcpy(new, msg->stream, sizeof *new);
+	return new;
+}
+
+
+void create_file(char *path,size_t size) {
+
+	FILE *f = fopen(path, "wb");
+
+	fseek(f,size-1,SEEK_SET);
+
+	fputc('\n', f);
+
+	fclose(f);
+}
+
+
+void clean_file(char *path) {
+
+	FILE *f = fopen(path, "wb");
+
+	fclose(f);
+}
+
+
+char* read_file(char *path, size_t size) {
+
+	FILE *f = fopen(path, "rb");
+	if(f == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+
+	char *buffer = malloc(size + 1);
+	if(buffer == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	fread(buffer, size, 1, f);
+
+	fclose(f);
+
+	buffer[size] = '\0';
+
+	return buffer;
+}
+
+
+void memcpy_from_file(char *dest, char *path, size_t size) {
+	
+	FILE *f = fopen(path, "rb");
+
+	if(f != NULL) {
+		fread(dest, size, 1, f);
+		fclose(f);
+	}
+}
+
+
+char *read_file_and_clean(char *path, size_t size) {
+
+	FILE *f = fopen(path, "rb");
+	if(f == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+
+	char *buffer = malloc(size + 1);
+	if(buffer == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	fread(buffer, size, 1, f);
+
+	fclose(f);
+
+	f = fopen(path, "wb");
+
+	fclose(f);
+
+	buffer[size] = '\0';
+
+	return buffer;
+}
+
+
+char *read_whole_file(char *path) {
+
+	FILE *f = fopen(path, "rb");
+	if(f == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+
+	fseek(f, 0, SEEK_END);
+	long fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	char *buffer = malloc(fsize + 1);
+	if(buffer == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	fread(buffer, fsize, 1, f);
+
+	fclose(f);
+
+	buffer[fsize] = '\0';
+
+	return buffer;
+}
+
+
+char *read_whole_file_and_clean(char *path) {
+
+	FILE *f = fopen(path, "rb");
+	if(f == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+
+	fseek(f, 0, SEEK_END);
+	long fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	char *buffer = malloc(fsize + 1);
+	if(buffer == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	fread(buffer, fsize, 1, f);
+
+	fclose(f);
+
+	buffer[fsize] = '\0';
+
+	return buffer;
+}
+
+
+void write_file(char *path, char *data, size_t size) {
+
+	FILE *f = fopen(path, "wb");
+
+	fwrite(data, 1, size, f);
+
+	fclose(f);
+}
+
+
+void print_msg(t_msg *msg)
+{
+	int i;
+	puts("\n==================================================");
+	printf("CONTENIDOS DEL MENSAJE:\n");
+	printf("- ID: %s\n", id_string(msg->header.id));
+
+	for (i = 0; i < msg->header.argc; i++) {;
+		printf("- ARGUMENTO %d: %d\n", i + 1, msg->argv[i]);
+	}
+
+	printf("- TAMAÑO: %d\n", msg->header.length);
+	printf("- CUERPO: ");
+
+	for (i = 0; i < msg->header.length; i++)
+		putchar(*(msg->stream + i));
+	puts("\n==================================================\n");
+}
+
+char *id_string(t_msg_id id)
+{
+	char *buf;
+	switch(id) {
+		case NO_NEW_ID:
+			buf = strdup("NO_NEW_ID");
+			break;
+		case INIT_CONSOLE:
+			buf = strdup("INIT_CONSOLE");
+			break;
+		case KILL_CONSOLE:
+			buf = strdup("KILL_CONSOLE");
+			break;
+		case NUMERIC_INPUT:
+			buf = strdup("NUMERIC_INPUT");
+			break;
+		case NUMERIC_OUTPUT:
+			buf = strdup("NUMERIC_OUTPUT");
+			break;
+		case REPLY_STRING_INPUT:
+			buf = strdup("REPLY_STRING_INPUT");
+			break;
+		case REPLY_NUMERIC_INPUT:
+			buf = strdup("REPLY_NUMERIC_INPUT");
+			break;
+		case STRING_INPUT:
+			buf = strdup("STRING_INPUT");
+			break;
+		case STRING_OUTPUT:
+			buf = strdup("STRING_OUTPUT");
+			break;
+		case CREATE_SEGMENT:
+			buf = strdup("CREATE_SEGMENT");
+			break;
+		case OK_CREATE:
+			buf = strdup("OK_CREATE");
+			break;
+		case FULL_MEMORY:
+			buf = strdup("FULL_MEMORY");
+			break;
+		case INVALID_SEG_SIZE:
+			buf = strdup("INVALID_SEG_SIZE");
+			break;
+		case MAX_SEG_NUM_REACHED:
+			buf = strdup("MAX_SEG_NUM_REACHED");
+			break;
+		case WRITE_MEMORY:
+			buf = strdup("WRITE_MEMORY");
+			break;
+		case OK_WRITE:
+			buf = strdup("OK_WRITE");
+			break;
+		case SEGMENTATION_FAULT:
+			buf = strdup("SEGMENTATION_FAULT");
+			break;
+		case INVALID_DIR:
+			buf = strdup("INVALID_DIR");
+			break;
+		case DESTROY_SEGMENT:
+			buf = strdup("DESTROY_SEGMENT");
+			break;
+		case OK_DESTROY:
+			buf = strdup("OK_DESTROY");
+			break;
+		case REQUEST_MEMORY:
+			buf = strdup("REQUEST_MEMORY");
+			break;
+		case OK_REQUEST:
+			buf = strdup("OK_REQUEST");
+			break;
+		case NEXT_TCB:
+			buf = strdup("NEXT_TCB");
+			break;
+		case CPU_TCB:
+			buf = strdup("CPU_TCB");
+			break;
+		case RETURN_TCB:
+			buf = strdup("RETURN_TCB");
+			break;
+		case CPU_ABORT:
+			buf = strdup("CPU_ABORT");
+			break;
+		case FINISHED_THREAD:
+			buf = strdup("FINISHED_THREAD");
+			break;
+		case CPU_CONNECT:
+			buf = strdup("CPU_CONNECT");
+			break;
+		case CPU_INTERRUPT:
+			buf = strdup("CPU_INTERRUPT");
+			break;
+		case CPU_CREA:
+			buf = strdup("CPU_CREA");
+			break;
+		case CPU_JOIN:
+			buf = strdup("CPU_JOIN");
+			break;
+		case CPU_BLOCK:
+			buf = strdup("CPU_BLOCK");
+			break;
+		case CPU_WAKE:
+			buf = strdup("CPU_WAKE");
+			break;
+		default:
+			buf = string_from_format("%d, <AGREGAR A LA LISTA>", id);
+			break;
+	}
+
+	return buf;
+}
+
+
+void print_tcb(t_hilo *tcb)
+{
+	int i;
+	puts("CONTENIDOS DEL TCB");
+	printf("Registro PID Valor:		%8d\n", tcb->pid);
+	printf("Registro TID Valor:		%8d\n", tcb->tid);
+	printf("Registro KM Valor:		%8d\n", tcb->kernel_mode);
+	printf("Registro CS Valor:		%8d\n", tcb->segmento_codigo);
+	printf("Registro CS_Size Valor:		%8d\n", tcb->segmento_codigo_size);
+	printf("Registro IP Valor:		%8d\n", tcb->puntero_instruccion);
+	printf("Registro Stack Valor:		%8d\n", tcb->base_stack);
+	printf("Registro Stack_Size Valor:	%8d\n", tcb->cursor_stack);
+
+	for(i = 0;i < 5; ++i)
+		printf("Registro %c. Valor:		%8d\n",('A'+i), tcb->registros[i]);
+	printf("Registro COLA:			%8d\n", tcb->cola);
+	puts("\n");
+}
+
+
+char *string_cola(t_cola cola)
+{
+	switch(cola) {
+		case NEW:
+			return "NEW";
+		case READY:
+			return "READY";
+		case BLOCK:
+			return "BLOCK";
+		case EXEC:
+			return "EXEC";
+		case EXIT:
+			return "EXIT";
+		default:
+			return NULL;
+	}
+}
---
+++ MilanesaLibrary/utiles/utiles/utiles.h
@@ -0,0 +1,287 @@
+#ifndef UTILES_H_
+#define UTILES_H_
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <commons/string.h>
+#include <panel/panel.h>
+
+#define REG_SIZE 4
+
+/* Funciones Macro */
+
+/*
+ * Compara dos numeros y retorna el mínimo.
+ */
+#define min(n, m) (n < m ? n : m)
+
+/*
+ * Compara dos numeros y retorna el máximo.
+ */
+#define max(n, m) (n > m ? n : m)
+
+/*
+ * Sleep en microsegundos.
+ */
+#define msleep(usecs) usleep(usecs * 1000)
+
+/*
+ * Alternativa sin undefined behavior a fflush(STDIN) para usar con scanf().
+ */
+#define clean_stdin_buffer() scanf("%*[^\n]")
+
+/*
+ * RNG. Retorna valores entre 0 y limit.
+ */
+#define randomize(limit) (rand() % (limit + 1))
+
+/*
+ * Divide dos enteros redondeando hacia arriba.
+ */
+#define divRoundUp(n,m) (n % m == 0 ? n / m : n / m + 1)
+
+/* FIN Funciones Macro */
+
+/****************** IDS DE MENSAJES. ******************/
+
+typedef enum {
+	NO_NEW_ID,					/* Valor centinela para evitar la modificación de id en modify_message(). */
+
+	INIT_CONSOLE,				/* Pedido de creación de hilo principal de Consola a Kernel. */
+	KILL_CONSOLE,				/* Respuesta de finalización por error de Kernel a Consola. */
+
+	CPU_CONNECT,				/* Pedido de conexión de CPU a Kernel. */
+
+	CPU_TCB,					/* Pedido de TCB de CPU a Kernel. */
+	NEXT_TCB,					/* Envío de TCB de Kernel a CPU. */
+
+	RETURN_TCB,					/* Retorno de TCB de CPU a Kernel. */
+
+	FINISHED_THREAD,			/* Envío de TCB, cuya ejecución ha finalizado, de CPU a Kernel. */
+
+	CPU_ABORT,					/* Pedido de la CPU al Kernel para abortar la ejecución de un hilo */
+
+	/****************** SERVICIOS EXPUESTOS A CPU: INTERRUPCIÓN. ******************/ 
+	CPU_INTERRUPT,				/* Pedido de interrupción de un hilo de proceso de CPU a Kernel. */
+
+	/****************** SERVICIOS EXPUESTOS A CPU: ENTRADA ESTÁNDAR. ******************/
+	NUMERIC_INPUT,				/* Pedido de input numérico de CPU. */
+	STRING_INPUT,				/* Pedido de input de string de CPU. */
+	REPLY_NUMERIC_INPUT,		/* Respuesta de numeric input de Consola. */
+	REPLY_STRING_INPUT,			/* Respuesta de string input de Consola. */
+
+	/****************** SERVICIOS EXPUESTOS A CPU: SALIDA ESTÁNDAR. ******************/
+	NUMERIC_OUTPUT,				/* Pedido de output numérico de CPU. */
+	STRING_OUTPUT,				/* Pedido de output de string de CPU. */
+
+	/****************** SERVICIOS EXPUESTOS A CPU: CREAR HILO. ******************/
+	CPU_CREA,					/* Pedido de nuevo hilo de proceso de CPU a Kernel. */
+	CREA_OK,					/* Respuesta de hilo creado. */
+	CREA_FAIL,					/* Respuesta de fallo al crear hilo. */
+
+	/****************** SERVICIOS EXPUESTOS A CPU: JOIN. ******************/
+	CPU_JOIN,					/* Pedido de unión a hilo de proceso de CPU a Kernel. */
+
+	/****************** SERVICIOS EXPUESTOS A CPU: BLOQUEAR. ******************/
+	CPU_BLOCK,					/* Pedido de bloqueo de hilo de proceso por recurso de CPU a Kernel. */
+
+	/****************** SERVICIOS EXPUESTOS A CPU: DESPERTAR. ******************/
+	CPU_WAKE,					/* Pedido de desbloqueo hilo de proceso bloqueado por recurso de CPU a Kernel. */
+
+
+	/****************** INTERFAZ MSP: RESERVAR SEGMENTO. ******************/
+	CREATE_SEGMENT,				/* Pedido de creación de segmento a MSP. */
+	OK_CREATE,					/* Respuesta de segmento creado de MSP. */
+	FULL_MEMORY,				/* Respuesta de memoria llena. */
+	INVALID_SEG_SIZE,			/* Respuesta de tamaño de segmento inválido. */
+	MAX_SEG_NUM_REACHED,		/* Respuesta de máxima cantidad de segmentos del proceso alcanzada. */
+
+	/****************** INTERFAZ MSP: DESTRUIR SEGMENTO. ******************/
+	DESTROY_SEGMENT,			/* Pedido de destrucción de segmento a MSP. */
+	OK_DESTROY,					/* Respuesta de eliminado correcto de MSP. */
+
+	/****************** INTERFAZ MSP: SOLICITAR MEMORIA. ******************/
+	REQUEST_MEMORY,				/* Pedido de datos a la MSP. */
+	OK_REQUEST,					/* Respuesta de lectura correcta de MSP. */
+
+	/****************** INTERFAZ MSP: ESCRIBIR MEMORIA. ******************/
+	WRITE_MEMORY,				/* Pedido de escritura en memoria a MSP. */
+	OK_WRITE,					/* Respuesta de escritura correcta de MSP. */
+
+	/****************** INTERFAZ MSP: SOLICITAR/ESCRIBIR MEMORIA. ******************/
+	INVALID_DIR,				/* Respuesta de dirección inválida. */
+	SEGMENTATION_FAULT			/* Respuesta de error de segmento en lectura/escritura de memoria. */
+
+} t_msg_id;
+
+
+/****************** ESTRUCTURAS DE DATOS. ******************/
+
+typedef struct {
+	t_msg_id id;
+	uint32_t length;
+	uint16_t argc;
+} __attribute__ ((__packed__)) t_header;
+
+typedef struct {
+	t_header header;
+	char *stream;
+	int32_t *argv;
+} __attribute__ ((__packed__)) t_msg;
+
+
+/****************** FUNCIONES SOCKET. ******************/
+
+/*
+ * Crea, vincula y escucha un socket desde un puerto determinado.
+ */
+int server_socket(uint16_t port);
+
+/*
+ * Crea y conecta a una ip:puerto determinado.
+ */
+int client_socket(char* ip, uint16_t port);
+
+/*
+ * Acepta la conexion de un socket.
+ */
+int accept_connection(int sock_fd);
+
+/*
+ * Recibe un t_msg a partir de un socket determinado.
+ */
+t_msg *recibir_mensaje(int sock_fd);
+
+/*
+ * Envia los contenidos de un t_msg a un socket determinado.
+ */
+int enviar_mensaje(int sock_fd, t_msg *msg);
+
+
+/****************** FUNCIONES T_MSG. ******************/
+
+/*
+ * Crea un t_msg sin argumentos, a partir del id.
+ */
+t_msg *id_message(t_msg_id id);
+
+/*
+ * Crea un t_msg a partir de count argumentos.
+ */
+t_msg *argv_message(t_msg_id id, uint16_t count, ...);
+
+/*
+ * Crea un t_msg a partir de un string y count argumentos.
+ */
+t_msg *string_message(t_msg_id id, char *message, uint16_t count, ...);
+
+/*
+ * Agrega nuevos argumentos a un mensaje (estilo FIFO).
+ */
+t_msg *modify_message(t_msg_id new_id, t_msg *old_msg, uint16_t new_count, ...);
+
+/*
+ * Elimina todos los argumentos existentes de un mensaje y agrega nuevos.
+ */
+t_msg *remake_message(t_msg_id new_id, t_msg *old_msg, uint16_t new_count, ...);
+
+/*
+ * Crea un t_msg a partir de los contenidos de un archivo beso y count argumentos.
+ */
+t_msg *beso_message(t_msg_id id, char *beso_path, uint16_t count, ...);
+
+/*
+ * Crea un t_msg a partir de los contenidos de un tcb y count argumentos.
+ */
+t_msg *tcb_message(t_msg_id id, t_hilo *tcb, uint16_t count, ...);
+
+/*
+ * Libera los contenidos de un t_msg.
+ */
+void destroy_message(t_msg *mgs);
+
+
+/****************** FUNCIONES FILE SYSTEM. ******************/
+
+/*
+ * Crea un archivo de size bytes de tamaño.
+ */
+void create_file(char *path,size_t size);
+
+/*
+ * Vacía el archivo indicado por path. Si no existe lo crea.
+ */
+void clean_file(char *path);
+
+/*
+ * Lee un archivo y retorna los primeros size bytes de su contenido.
+ */
+char* read_file(char *path, size_t size);
+
+/*
+ * Si existe, copia el contenido del archivo path en dest.
+ */
+void memcpy_from_file(char *dest,char *path,size_t size);
+
+/*
+ * Elimina los primeros size bytes del archivo path, y los retorna.
+ */
+char* read_file_and_clean(char *path, size_t size);
+
+/*
+ * Lee un archivo y retorna todo su contenido.
+ */
+char* read_whole_file(char *path);
+
+/*
+ * Lee un archivo y retorna todo su contenido, vaciándolo.
+ */
+char* read_whole_file_and_clean(char *path);
+
+/*
+ * Abre el archivo indicado por path (si no existe lo crea) y escribe size bytes de data.
+ */
+void write_file(char *path,char* data,size_t size);
+
+
+/****************** FUNCIONES AUXILIARES. ******************/
+
+/*
+ * Genera una nueva secuencia de enteros pseudo-random a retornar por rand().
+ */
+void seedgen(void);
+
+/*
+ * Muestra los contenidos y argumentos de un t_msg.
+ */
+void print_msg(t_msg *msg);
+
+/*
+ * Convierte t_msg_id a string.
+ */
+char *id_string(t_msg_id id);
+
+/*
+ * Recupera los contenidos de un tcb cargado a mensaje.
+ */
+t_hilo *retrieve_tcb(t_msg *msg);
+
+/*
+ * Convierte t_cola a string.
+ */
+char *string_cola(t_cola cola);
+
+
+void print_tcb(t_hilo *tcb);
+
+#endif /* UTILES_H_ */
