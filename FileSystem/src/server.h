e0df30a41490af5d1e5ff4e0a61cdb4c1165651c
Tomas Yagas <tomas.yagas@gmail.com>
2015-07-06 18:18:54 -0300

    termino funcionar de copiar bloques entre nodos

--- FileSystem/src/server.h
+++ FileSystem/src/server.h
@@ -48,6 +48,7 @@ void desconexion_nodo(int socket);
 void desconexion_marta(int socket);
 void enviar_fs_no_operativo(int socket);
 char* mensaje_get_bloque(void* argumentos);
+int mensaje_set_bloque(void* argumentos);
 t_msg* mensaje_info_archivo(char* ruta_archivo);
 t_msg* mensaje_copiar_archivo_temporal_a_mdfs(char* mensaje);
 
